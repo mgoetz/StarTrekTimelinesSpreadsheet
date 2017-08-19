ce3055bbb864672a3d3e5a99b22799b3b3fa2c42
IAmPicard <tekmanro@yahoo.com>
2017-08-19 03:19:25 -0700

    Added gauntlet crew selection

--- sttsheet.js
+++ sttsheet.js
@@ -30,6 +30,7 @@ const optionDefinitions = [
 	{ name: 'crew', alias: 'c', type: Boolean, defaultValue: true },
 	{ name: 'items', alias: 'i', type: Boolean, defaultValue: false },
 	{ name: 'ships', alias: 's', type: Boolean, defaultValue: false },
+	{ name: 'gauntlet', alias: 'g', type: Boolean, defaultValue: false },
 	{ name: 'accesstoken', type: String, defaultOption: true }
 ];
 
@@ -98,7 +99,7 @@ function matchCrew(crew_avatars, character, trait_names)
 		var crew = crew_avatars.find(function (archetype) { return archetype.id === id; });
 		return {
 			id: crew.id, name: crew.name, short_name: crew.short_name, max_rarity: crew.max_rarity,
-			level: 0, rarity: 0, frozen: 0, buyback: false, traits: '',
+			level: 0, rarity: 0, frozen: 0, buyback: false, traits: '', rawTraits: [],
 			command_skill: { 'core': 0, 'min': 0, 'max': 0 }, science_skill: { 'core': 0, 'min': 0, 'max': 0 },
 			security_skill: { 'core': 0, 'min': 0, 'max': 0 }, engineering_skill: { 'core': 0, 'min': 0, 'max': 0 },
 			diplomacy_skill: { 'core': 0, 'min': 0, 'max': 0 }, medicine_skill: { 'core': 0, 'min': 0, 'max': 0 }
@@ -123,6 +124,7 @@ function matchCrew(crew_avatars, character, trait_names)
 		}
 
 		rosterEntry.traits = crew.traits.concat(crew.traits_hidden).map(function (trait) { return trait_names[trait] ? trait_names[trait] : trait; }).join();
+		rosterEntry.rawTraits = crew.traits;
 		roster.push(rosterEntry);
 	});
 
@@ -139,9 +141,104 @@ function matchCrew(crew_avatars, character, trait_names)
 }
 
 // Function gets called when we're done downloading all the data we need
-function finishedLoading()
+function finishedLoading(token)
 {
-	if (options.items)
+	if (options.gauntlet)
+	{
+		const options = { method: 'GET', qs: { client_api: 7, access_token: token, gauntlet_id: -1 } };
+
+		options.uri = 'https://stt.disruptorbeam.com/gauntlet/status';
+		request(options, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var gauntlet = JSON.parse(body);
+
+				var currentGauntlet = gauntlet.character.gauntlets[0];
+				if (currentGauntlet.state != 'NONE')
+				{
+					console.log('It looks like you already started this gauntlet! Try to use this option before joining your next gauntlet');
+					return;
+				}
+
+				console.log('Calculating best crew for next gauntlet, starting in ' + currentGauntlet.seconds_to_join / 60 + ' minutes...');
+				console.log('Featured skill: ' + currentGauntlet.contest_data.featured_skill);
+				console.log('Traits: ' + currentGauntlet.contest_data.traits.join());
+
+				var allItems = matchCrew(allcrew.crew_avatars, player.player.character, config.config.trait_names);
+
+				// TODO: magical numbers below; tune these (or get them as parameters for customization)
+				var featuredSkillBonus = 1.1;
+				var critBonusDivider = 3;
+
+				const skillList = [
+					'command_skill',
+					'science_skill',
+					'security_skill',
+					'engineering_skill',
+					'diplomacy_skill',
+					'medicine_skill'];
+
+				var gauntletCrew = allItems.map(function (crew) {
+					var newCrew = { id: crew.id, name: crew.name, crit: 5 };
+
+					skillList.forEach(function (skill) {
+						newCrew[skill] = crew[skill].min + crew[skill].max;
+					});
+
+					newCrew[currentGauntlet.contest_data.featured_skill] = newCrew[currentGauntlet.contest_data.featured_skill] * featuredSkillBonus;
+
+					currentGauntlet.contest_data.traits.forEach(function (trait) {
+						if (crew.rawTraits.includes(trait))
+							newCrew.crit += currentGauntlet.contest_data.crit_chance_per_trait;
+					});
+
+					skillList.forEach(function (skill) {
+						newCrew[skill] = newCrew[skill] * (100 + newCrew.crit / critBonusDivider) / 100;
+					});
+
+					return newCrew;
+				});
+
+				var sortedCrew = [];
+
+				function getScore(gauntletCrewItem, maxSkill)
+				{
+					score = gauntletCrewItem[maxSkill]; // double account for preferred skill
+					skillList.forEach(function (skill) {
+						score += gauntletCrewItem[skill];
+					});
+
+					return score;
+				}
+
+				skillList.forEach(function (skill) {
+					gauntletCrew.sort(function (a, b) {
+						return b[skill] - a[skill];
+					});
+					console.log('Best overall for ' + skill + ' : ' + gauntletCrew[0].name)
+
+					// Get the first 2 in the final score sheet
+					sortedCrew.push({ 'id': gauntletCrew[0].id, 'name': gauntletCrew[0].name, 'score': getScore(gauntletCrew[0], skill) });
+					sortedCrew.push({ 'id': gauntletCrew[1].id, 'name': gauntletCrew[1].name, 'score': getScore(gauntletCrew[1], skill) });
+				});
+
+				sortedCrew.sort(function (a, b) {
+					return b.score - a.score;
+				});
+
+				// Remove duplicates
+				var seen = {};
+				sortedCrew = sortedCrew.filter(function (item) {
+					return seen.hasOwnProperty(item.id) ? false : (seen[item.id] = true);
+				});
+
+				// Get the first 5
+				sortedCrew = sortedCrew.slice(0, 5);
+
+				console.log('Based on the probabilistic algorithm my recommended selection is: ' + sortedCrew.map(function (crew) { return crew.name }).join(', '));
+			}
+		});
+	}
+	else if (options.items)
 	{
 		// Return details about the items
 		var fields = [
@@ -222,7 +319,7 @@ getAccessToken(function(token)
 		{
 			player = JSON.parse(body);
 			if (--waiting == 0)
-				finishedLoading();
+				finishedLoading(token);
 		}
 	});
 
@@ -232,7 +329,7 @@ getAccessToken(function(token)
 		{
 			config = JSON.parse(body);
 			if (--waiting == 0)
-				finishedLoading();
+				finishedLoading(token);
 		}
 	});
 
@@ -242,7 +339,7 @@ getAccessToken(function(token)
 		{
 			allcrew = JSON.parse(body);
 			if (--waiting == 0)
-				finishedLoading();
+				finishedLoading(token);
 		}
 	});
 });
