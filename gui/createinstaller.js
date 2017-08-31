bfbcb84556b66e1b9b425b761bd735824a281d76
IAmPicard <tekmanro@yahoo.com>
2017-08-30 18:43:16 -0700

    Update installer script to use version from package file.

--- gui/createinstaller.js
+++ gui/createinstaller.js
@@ -1,17 +1,20 @@
-const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
-const path = require('path')
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller;
+const path = require('path');
+const fs = require('fs');
+
+var version = JSON.parse(fs.readFileSync('package.json'), 'utf8').version;
 
 getInstallerConfig()
 	.then(createWindowsInstaller)
 	.catch((error) => {
 		console.error(error.message || error)
 		process.exit(1)
-	})
+	});
 
 function getInstallerConfig() {
-	console.log('creating windows installer')
-	const rootPath = path.join('./')
-	const outPath = path.join(rootPath, 'builds')
+	console.log('creating windows installer');
+	const rootPath = path.join('./');
+	const outPath = path.join(rootPath, 'builds');
 
 	return Promise.resolve({
 		appDirectory: path.join(outPath, 'startrektimelinestool-win32-x64'),
@@ -19,6 +22,6 @@ function getInstallerConfig() {
 		noMsi: false,
 		outputDirectory: path.join(outPath, 'windows-installer'),
 		exe: 'startrektimelinestool.exe',
-		setupExe: 'startrektimelinestoolinstaller.exe'
-	})
+		setupExe: 'setup-startrektimelinestool-' + version + '.exe'
+	});
 }
