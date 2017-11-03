b8017869d127242f2077589d10afde6c2e2171b0
IAmPicard <tekmanro@yahoo.com>
2017-11-02 19:34:23 -0700

    Start implementing crew recommendations for voyage. Doesn't really work yet.

--- src/components/CrewRecommendations.js
+++ src/components/CrewRecommendations.js
@@ -110,6 +110,139 @@ export class MinimalComplement extends React.Component {
 	}
 }
 
+export class VoyageCrew extends React.Component {
+	constructor(props) {
+		super(props);
+
+		let voyage = STTApi.playerData.character.voyage[0];
+		if (voyage.state != 'unstarted') {
+			// Best ship
+			let consideredShips = [];
+			STTApi.ships.forEach(ship => {
+				if (ship.id > 0) {
+					let entry = {
+						ship: ship,
+						score: ship.antimatter
+					};
+
+					if (ship.traits.find(trait => trait == voyage.ship_trait)) {
+						entry.score += 150;
+					}
+
+					consideredShips.push(entry);
+				}
+			});
+
+			// TODO: Figure out how the antimatter is actually calculated in the client (2050 to 2500)
+			consideredShips = consideredShips.sort((a, b) => b.score - a.score);
+			consideredShips = consideredShips.filter(entry => entry.score == consideredShips[0].score);
+
+			// Best crew
+
+			// TODO: what would be the most efficient algorithm?
+			// If we do a full combinatorial we'd use a lot of resources numcrew^12 (potentially 400^12 entries, way too much)
+			// Tried that on a Xeon and with 200 crew it takes >1hr to calculate; obviously not a solution
+			// Lets randomly pick just the first X crew sorted by the given skill, should make it more manageable
+			// Even 10 would be too much though; 10^12 = 1000000000000
+			const howManyToConsider = 3;
+			let bestChoices = [];
+			let bestScore = 0;
+
+			function crewScore(crew, primary_skill, secondary_skill) {
+				let score = 0;
+				Object.keys(CONFIG.SKILLS).forEach(skill => {
+					let skillScore = crew[skill].core + (crew[skill].max - crew[skill].min) / 2;
+					score += skillScore;
+					if (skill == primary_skill) {
+						score += skillScore * 4;
+					}
+					if (skill == secondary_skill) {
+						score += skillScore * 3;
+					}
+				});
+
+				return score;
+			}
+
+			let presortedCrewSlices = {};
+			Object.keys(CONFIG.SKILLS).forEach(skill => {
+				presortedCrewSlices[skill] = STTApi.roster.sort((a, b) => crewScore(b, undefined, skill) - crewScore(a, undefined, skill))
+					.filter(crew => crew[skill].core > 0)
+					.slice(0, howManyToConsider);
+			});
+
+			let currentChoices = [];
+			function fillSlot(slotIndex) {
+				presortedCrewSlices[voyage.crew_slots[slotIndex].skill].forEach(choice => {
+					if (currentChoices.find(v => v.choice.id == choice.id)) {
+						// If already in the list of choices, skip
+						return;
+					}
+
+					let currentChoice = {
+						choice,
+						score: crewScore(choice, voyage.skills.primary_skill, voyage.skills.secondary_skill)
+					};
+
+					if (choice.rawTraits.find(trait => trait == voyage.crew_slots[slotIndex].trait)) {
+						currentChoice.score *= 1.1; //TODO: Fine-tune this value
+					}
+
+					currentChoices.push(currentChoice);
+					if (slotIndex < voyage.crew_slots.length - 1) {
+						fillSlot(slotIndex + 1);
+					}
+					else {
+						// we have a complete crew complement, compute score
+						let currentScore = currentChoices.reduce((sum, choice) => sum + choice.score, 0);
+						if (currentScore > bestScore) {
+							bestScore = currentScore;
+							bestChoices = currentChoices.slice();
+						}
+					}
+					currentChoices.pop();
+				});
+			}
+
+			fillSlot(0);
+
+			this.state = {
+				voyageRecommendations: {
+					consideredShips, bestChoices
+				}
+			};
+		}
+		else {
+			this.state = {
+				voyageRecommendations: undefined
+			};
+		}
+	}
+
+	render() {
+		if (this.state.voyageRecommendations) {
+
+			let shipSpans = [];
+			this.state.voyageRecommendations.consideredShips.forEach(entry => {
+				shipSpans.push(<span key={entry.ship.id}>{entry.score} - {entry.ship.name}</span>);
+			});
+
+			let crewSpans = [];
+			this.state.voyageRecommendations.bestChoices.forEach(entry => {
+				crewSpans.push(<span key={entry.choice.id}>{entry.score} - {entry.choice.name}</span>);
+			});
+
+			return (<CollapsibleSection title='Recommendations for next voyage'>
+				{shipSpans}
+				{crewSpans}
+			</CollapsibleSection>);
+		}
+		else {
+			return <span>Can only show voyage recommendations if you didn't begin your voyage yet!</span>;
+		}
+	}
+}
+
 export class CrewRecommendations extends React.Component {
 	constructor(props) {
 		super(props);
@@ -126,6 +259,7 @@ export class CrewRecommendations extends React.Component {
 				<GuaranteedSuccess title='Missions without guaranteed success' cadet={false} />
 				<CrewDuplicates title='Crew duplicates' />
 				<MinimalComplement title='Minimal crew complement needed for cadet challenges' />
+				<VoyageCrew />
 			</div>
 		);
 	}
